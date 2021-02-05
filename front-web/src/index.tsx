878a861ac32b7182137051d27dd6cf86fa65d1c6
souzaoe <souzaoe@hotmail.com>
2021-02-05 17:21:08 -0300

    Erro no custom

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
-import App from './App' 
+import App from './App'; 
+
 
 ReactDOM.render(
-  <h1>App</h1>,
+  <App />,
   document.getElementById('root')
 );
